1cc537dcb33b8ad2a0dbcfebd736c31f58485f18
ColeWill <wcole2222@gmail.com>
2025-08-11 17:05:47 -0600

    updated the addpost reducer with the prepare callback to generate the id in one place

--- src/features/posts/postsSlice.ts
+++ src/features/posts/postsSlice.ts
@@ -1,4 +1,4 @@
-import { createSlice, PayloadAction } from '@reduxjs/toolkit'
+import { createSlice, nanoid, PayloadAction } from '@reduxjs/toolkit'
 
 export interface Post {
   id: string
@@ -23,8 +23,15 @@ const postsSlice = createSlice({
   name: 'posts',
   initialState,
   reducers: {
-    postAdded: (state, action: PayloadAction<Post>) => {
-      state.push(action.payload)
+    postAdded: {
+      reducer(state, action: PayloadAction<Post>) {
+        state.push(action.payload)
+      },
+      prepare(title: string, content: string) {
+        return {
+          payload: { id: nanoid(), title, content },
+        }
+      },
     },
     postUpdated: (state, action: PayloadAction<Post>) => {
       // find the right post and mutate
